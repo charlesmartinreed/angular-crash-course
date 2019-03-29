49b8655bca8b977840b726fd5281a2cd35dd829b
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-29 18:19:49 -0500

    Add global CSS styling. Add todo-item component styling. Demonstrate basics of dynamic class binding with ngClass.

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+	// this input allows us to utilize our todos in the todoItem html - todo is an input with a type of Todo class.
+	@Input() todo: Todo;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+	// Set Dynamic Classes
+	setClasses() {
+		let classes = {
+			// set class and the condition under which it is applied
+			todo: true,
+			'is-complete': this.todo.completed
+		}
+
+		return classes;
+	}
+
+}
