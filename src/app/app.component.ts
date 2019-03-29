bf7d8e2c0645f075f624a21f8466bee54571c949
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-29 17:53:40 -0500

    Demonstrate basics of adding methods to components in app.component.ts.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	name = "Charles Martin Reed"
+	// recommended to use typescript
+	name: string = "Charles Martin Reed";
+
+	// because TS is strongly typed, this generates an error
+	// name: string = 12;
+
+	// we can also add methods inside of the component
+	constructor() {
+		this.changeName('Billy Blanks');
+		// executes as soon as the component loads
+		// console.log(this.name);
+		// console.log(123);
+	}
+
+	// :void is optional; means that the function doesn't return
+	changeName(name: string):void {
+		this.name = name;
+	}
 }
