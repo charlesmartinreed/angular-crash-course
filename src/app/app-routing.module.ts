d7a454dbfdfa60e294f828dc4b1a276c77c729b1
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-29 20:08:23 -0500

    Demonstrate basics of using the Angular router.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TodosComponent } from './components/todos/todos.component';
+import { AboutComponent } from './components/pages/about/about.component';
 
-const routes: Routes = [];
+
+// we need to import our routes, then add them to the routes array
+const routes: Routes = [
+	{ path: '', component: TodosComponent },
+	{ path: 'about', component: AboutComponent}
+];
+
+// then, in the main app component html we add <router-outlet>
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
